fd4b9def6b62246d7e556705c254c2ede64838b6
quynam <quynamle123@gmail.com>
2020-06-24 09:33:47 +0700

    fix setup

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,14 +1,19 @@
 // config redux
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from 'redux';
 
 //some Reducer
 import adminReducer from '../features/Admin/adminSlice';
 
 
 //root Reducer
-const rootReducer = {
-    admin: adminReducer,
-}
+// const rootReducer = {
+//     admin: adminReducer,
+// }
+
+const rootReducer = combineReducers({
+    adminReducer
+})
 
 //config store
 const store = configureStore({

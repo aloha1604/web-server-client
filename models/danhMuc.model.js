aac3dd265e7bf4ec6b6f7c0e3494682c66dfa583
quynam <quynamle123@gmail.com>
2020-07-06 12:09:10 +0700

    viet xong model danh muc and nhom

---
+++ models/danhMuc.model.js
@@ -0,0 +1,71 @@
+const con = require("./db");
+const moment = require('../lib/moment.lib');
+
+exports.getAllDanhMuc = (result) => {
+    const sql = "SELECT * FROM tbl_danhmuc"
+    con.query(sql, (err, res) => {
+        if (err) {
+            console.log("error:", err);
+            result(null, err);
+            return;
+        } else {
+            // console.log(res);
+            result(null, res);
+        }
+    })
+}
+exports.getAllDanhMucByIdDanhMuc = (danhmuc_id, result) => {
+    const sql = "SELECT * FROM tbl_danhmuc where danhmuc_id = ? "
+    con.query(sql, [danhmuc_id], (err, res) => {
+        if (err) {
+            console.log("error:", err);
+            result(null, err);
+            return;
+        } else {
+            // console.log(res);
+            result(null, res);
+        }
+    })
+}
+
+exports.add = (danhmuc_ten, result) => {
+    const sql = "INSERT INTO tbl_danhmuc (danhmuc_ten,create_at,update_at) VALUES (?,?,?)"
+    con.query(sql, [danhmuc_ten, moment.mysqlTimestamp, moment.mysqlTimestamp], (err, res) => {
+        if (err) {
+            console.log("error:", err);
+            result(null, err);
+            return;
+        } else {
+            // console.log(res);
+            result(null, res);
+        }
+    })
+}
+
+exports.remove = (danhmuc_id, result) => {
+    const sql = "DELETE FROM tbl_danhmuc WHERE danhmuc_id = ?"
+    con.query(sql, [danhmuc_id], (err, res) => {
+        if (err) {
+            console.log("error:", err);
+            result(null, err);
+            return;
+        } else {
+            // console.log(res);
+            result(null, res);
+        }
+    })
+}
+
+exports.update = (danhmuc_id, danhmuc_ten, result) => {
+    const sql = "UPDATE tbl_danhmuc set danhmuc_ten = ? WHERE danhmuc_id = ?"
+    con.query(sql, [danhmuc_ten, danhmuc_id], (err, res) => {
+        if (err) {
+            console.log("error:", err);
+            result(null, err);
+            return;
+        } else {
+            // console.log(res);
+            result(null, res);
+        }
+    })
+}
---
+++ models/nhom.model.js
@@ -0,0 +1,58 @@
+const con = require("./db");
+const moment = require('../lib/moment.lib');
+
+exports.getAllNhomByIdDanhMuc = (danhmuc_id, result) => {
+    const sql = "SELECT * FROM tbl_nhom where danhmuc_id=? "
+    con.query(sql, [danhmuc_id], (err, res) => {
+        if (err) {
+            console.log("error:", err);
+            result(null, err);
+            return;
+        } else {
+            // console.log(res);
+            result(null, res);
+        }
+    })
+}
+
+exports.add = (danhmuc_id, nhom_ten, result) => {
+    const sql = "INSERT INTO tbl_nhom (danhmuc_id,nhom_ten,create_at,update_at) VALUES (?,?,?,?)"
+    con.query(sql, [danhmuc_id, nhom_ten, moment.mysqlTimestamp, moment.mysqlTimestamp], (err, res) => {
+        if (err) {
+            console.log("error:", err);
+            result(null, err);
+            return;
+        } else {
+            // console.log(res);
+            result(null, res);
+        }
+    })
+}
+
+exports.remove = (nhom_id, result) => {
+    const sql = "DELETE FROM tbl_nhom WHERE nhom_id = ?"
+    con.query(sql, [nhom_id], (err, res) => {
+        if (err) {
+            console.log("error:", err);
+            result(null, err);
+            return;
+        } else {
+            // console.log(res);
+            result(null, res);
+        }
+    })
+}
+
+exports.update = (nhom_id, nhom_ten, result) => {
+    const sql = "UPDATE tbl_nhom set nhom_ten = ? WHERE nhom_id = ?"
+    con.query(sql, [nhom_ten, nhom_id], (err, res) => {
+        if (err) {
+            console.log("error:", err);
+            result(null, err);
+            return;
+        } else {
+            // console.log(res);
+            result(null, res);
+        }
+    })
+}

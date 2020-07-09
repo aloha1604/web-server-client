1d44d321936b448671c13db97c21511e2784831f
quynam <quynamle123@gmail.com>
2020-07-09 20:10:15 +0700

    xong module dang tin

--- controller/dangtin.controller.js
+++ controller/dangtin.controller.js
@@ -38,11 +38,41 @@ exports.getHinhAnh = (req, res, next) => {
 // post : dangtin body: tindang_
 exports.dangTin = async (req, res) => {
     //get du lieu
-    const dangTin_tieuDe = req.body.dangtin_tieude;
     const nhom_id = req.body.nhom_id;
+    const dangTin_tieuDe = req.body.dangtin_tieude;
+    const dangtin_gia = req.body.dangtin_gia;
+    const tindang_tukhoa = req.body.tindang_tukhoa;
+    const tindang_tinhthanh = req.body.tindang_tinhthanh;
+    const tindang_quanhuyen = req.body.tindang_quanhuyen;
+    const tindang_phuongxa = req.body.tindang_phuongxa;
+    const tindang_noidung = req.body.tindang_noidung;
+    const tindang_linkyoutube = req.body.tindang_linkyoutube;
+    const tindang_hoten = req.body.tindang_hoten;
+    const tindang_email = req.body.tindang_email;
+    const tindang_phone = req.body.tindang_phone;
+    const tindang_diachi = req.body.tindang_diachi;
+    const tindang_thoigianlienhe = req.body.tindang_thoigianlienhe;
+    const tindang_active = 0;
+    const tindang_vipham = 0;
+
     const dataTinDang1 = [];
     dataTinDang1.push(nhom_id)
     dataTinDang1.push(dangTin_tieuDe)
+    dataTinDang1.push(dangtin_gia)
+    dataTinDang1.push(tindang_tukhoa)
+    dataTinDang1.push(tindang_tinhthanh)
+    dataTinDang1.push(tindang_quanhuyen)
+    dataTinDang1.push(tindang_phuongxa)
+    dataTinDang1.push(tindang_noidung)
+    dataTinDang1.push(tindang_linkyoutube)
+    dataTinDang1.push(tindang_hoten)
+    dataTinDang1.push(tindang_email)
+    dataTinDang1.push(tindang_phone)
+    dataTinDang1.push(tindang_diachi)
+    dataTinDang1.push(tindang_thoigianlienhe)
+    dataTinDang1.push(tindang_active)
+    dataTinDang1.push(tindang_vipham)
+    console.log(dataTinDang1)
 
     try {
         const flagInsert = (data) => {
@@ -58,7 +88,7 @@ exports.dangTin = async (req, res) => {
         }
         var dataDangTin = await flagInsert(dataTinDang1);
 
-
+        console.log(dataDangTin.insertId);
         // xử lý hình ảnh
         const reqFiles = [];
         const url = req.protocol + '://' + req.get('host')
@@ -79,7 +109,7 @@ exports.dangTin = async (req, res) => {
             })
         }
 
-        var dataDangTin = await flagInsert(dataTinDang1);
+
         var dataHinhAnh = await flagInsertHinhAnhByIdTin(reqFiles);
 
 

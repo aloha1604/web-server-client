2683a50a691679fa9a44af9e9bd1351ff400126f
quynam <quynamle123@gmail.com>
2020-07-09 20:09:13 +0700

    da xong dangtin model

--- models/dangtin.model.js
+++ models/dangtin.model.js
@@ -2,9 +2,11 @@ const con = require("./db");
 const moment = require('../lib/moment.lib');
 // 17 muc can them
 exports.add = (tinDang, result) => {
-    // const data = tinDang.push(moment.mysqlTimestamp);
-    // data = tinDang.push(moment.mysqlTimestamp);?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?:,tindang_gia,tindang_tukhoa,tindang_tinhthanh,tindang_quanhuyen,tindang_phuongxa,tindang_noidung,tindang_linkyoutube,tindang_hoten,tindang_phone,tindang_email,tindang_diachi,tindang_thoigianlienhe,tindang_active,tindang_vipham,create_at,update_at
-    const sql = "INSERT INTO tbl_tindang (nhom_id,tindang_tieude) VALUES (?,?)"
+    tinDang.push(moment.mysqlTimestamp);
+    tinDang.push(moment.mysqlTimestamp);
+    console.log(tinDang);
+    //;?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?:,tindang_gia,tindang_tukhoa,tindang_tinhthanh,tindang_quanhuyen,tindang_phuongxa,tindang_noidung,tindang_linkyoutube,tindang_hoten,tindang_phone,tindang_email,tindang_diachi,tindang_thoigianlienhe,tindang_active,tindang_vipham,create_at,update_at
+    const sql = "INSERT INTO tbl_tindang (nhom_id,tindang_tieude,tindang_gia,tindang_tukhoa,tindang_tinhthanh,tindang_quanhuyen,tindang_phuongxa,tindang_noidung,tindang_linkyoutube,tindang_hoten,tindang_email,tindang_phone,tindang_diachi,tindang_thoigianlienhe,tindang_active,tindang_vipham,create_at,update_at) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
     con.query(sql, tinDang, (err, res) => {
         if (err) {
             console.log("error:", err);

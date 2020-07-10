7182c5ad9f858c59d84ad2ebd7523cc73ea2bc23
quynam <quynamle123@gmail.com>
2020-07-10 15:47:40 +0700

    them dangtin slice vao store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -7,6 +7,7 @@ import adminAuth from '../features/Admin/components/Auth/authSlice';
 import danhMuc from '../features/Admin/components/DanhMucSanPham/danhMucSlice';
 import nhom from '../features/Admin/components/NhomSanPham/nhomSlice';
 import user from '../features/Admin/components/QuanLyUser/userSlice';
+import tinDang from '../features/Admin/components/QuanLyTinDang/dangTinSlice';
 
 //some Reducer client
 import userAuth from '../features/ClientUser/reducer/userSlice';
@@ -17,7 +18,7 @@ import phuongXa from '../features/ClientUser/reducer/apiPhuongXaSlice';
 
 //root Reducer
 const rootReducer = combineReducers({
-    adminAuth, danhMuc, nhom, user, userAuth, tinhThanh, quanHuyen, phuongXa
+    adminAuth, danhMuc, nhom, user, tinDang, userAuth, tinhThanh, quanHuyen, phuongXa
 })
 
 //config store

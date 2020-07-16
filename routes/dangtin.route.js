bb4fbb98e855405e815aefbef83206f4cc3a8fc1
quynam <quynamle123@gmail.com>
2020-07-16 13:47:41 +0700

    getOneTinByIdTinDAn

--- routes/dangtin.route.js
+++ routes/dangtin.route.js
@@ -8,6 +8,7 @@ router.get('/getAllTinViPham', dangTinController.getAllTinViPham);
 router.get('/getAllTinDaDuyet', dangTinController.getAllTinDaDuyet);
 router.get('/getTinMoi', dangTinController.getTinMoi);
 router.get('/getTinByIdNhom/:nhom_id/:page', dangTinController.getTinByIdNhom);
+router.get('/getOneTinByIdTinDang/:tindang_id', dangTinController.getOneTinByIdTinDang);
 
 module.exports = router;
 

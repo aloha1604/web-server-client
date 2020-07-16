55c1a8fdc9110e9fcb946a2e2c28f2dbfecfa73e
quynam <quynamle123@gmail.com>
2020-07-16 11:41:05 +0700

    add controller dang tin getinmoi

--- controller/dangtin.controller.js
+++ controller/dangtin.controller.js
@@ -268,6 +268,51 @@ exports.getAllTinDaDuyet = (req, res) => {
     })
 }
 
+exports.getTinMoi = (req, res) => {
+    dangTinModel.getTinMoi((err, data) => {
+        if (err) {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving customers."
+            });
+        } else {
+
+            // res.json({ dataTin: data });
+            let dataTin = [...data];
+            hinhAnhModel.getHinhAnh((err, dataa) => {
+                if (err) {
+                    res.status(500).send({
+                        message:
+                            err.message || "Some error occurred while retrieving customers."
+                    });
+                } else {
+                    let dataHinhAnh = [...dataa];
+                    var hinhAnh = [];
+
+                    let datanew = dataTin.map(tin => {
+                        let copytin = { ...tin }
+                        hinhAnh = [];
+                        copytin.hinhanh = [];
+                        copytin.hinhanh.length = 0;
+                        hinhAnh.length = 0;
+                        for (let i = 0; i < dataHinhAnh.length; i++) {
+                            if (copytin.tindang_id === dataHinhAnh[i].tindang_id) {
+                                hinhAnh.push(dataHinhAnh[i].hinhanh_ten)
+                            }
+                        }
+
+                        copytin.hinhanh = [...hinhAnh];
+                        return { ...copytin };
+                    })
+
+                    res.json({ dataTin: datanew });
+
+                }
+            })
+        }
+    })
+}
+
 exports.updateTinDangActive = async (req, res) => {
     let tinDang_id = req.params.tindang_id;
 
